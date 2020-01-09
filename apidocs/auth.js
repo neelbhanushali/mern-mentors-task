e7e3b908057c764b6901486b144ac06e32f02191
Neel Bhanushali <neal.bhanushali@gmail.com>
2020-01-09 22:04:33 +0530

    apidoc register

---
+++ apidocs/auth.js
@@ -0,0 +1,14 @@
+/**
+ * @api {POST} api/v1/auth/register Register
+ * @apiName Register
+ * @apiGroup Auth
+ * @apiVersion 1.0.0
+ *
+ * @apiParam {String} first_name
+ * @apiParam {String} [middle_name]
+ * @apiParam {String} last_name
+ * @apiParam {Date} dob
+ * @apiParam {Email} email
+ * @apiParam {String} password
+ * @apiParam {String} password_confirmation
+ */
