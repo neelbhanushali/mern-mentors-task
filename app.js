2d7ecbdf18303c168f58798580ac92c9efafd1c9
Neel Bhanushali <neal.bhanushali@gmail.com>
2020-01-09 22:05:12 +0530

    apidoc static

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ require('dotenv').config()
 
 // mongoose
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.DB_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
@@ -17,8 +21,16 @@ require("./global");
 const express = require("express");
 const app = express();
 
+// body parser
+const bodyparser = require("body-parser");
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
 // routing
 app.use("/", reqlib("routes"));
+
+// static
+app.use(express.static("public"));
 
 // server
 const server = app.listen(process.env.SERVER_PORT, function () {
