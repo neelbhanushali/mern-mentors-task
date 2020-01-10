7385fad4a90a844e2ddc46246498f3b8e185b977
Neel Bhanushali <neal.bhanushali@gmail.com>
2020-01-10 09:35:17 +0530

    paginator added

--- app/controllers/v1/AdminController.js
+++ app/controllers/v1/AdminController.js
@@ -2,7 +2,7 @@ const UserModel = reqlib('app/models/UserModel');
 
 module.exports = {
     async userList(req, res) {
-        const users = await UserModel.find({ is_admin: false })
+        const users = await UserModel.paginate({ is_admin: false })
 
         return Responder.success(res, users)
     }
