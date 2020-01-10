ca9cb96b1f31f558cd90ae9a907fc016a43371b0
Neel Bhanushali <neal.bhanushali@gmail.com>
2020-01-10 09:24:08 +0530

    admin middleware updated

--- app/middlewares/AdminMiddleware.js
+++ app/middlewares/AdminMiddleware.js
@@ -4,7 +4,7 @@ module.exports = (req, res, next) => {
     const token = bearerToken(req);
     jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
         if (!res.locals.decoded.is_admin) {
-            return Responder.forbidden(res);
+            return Responder.forbidden(res, 'You need to be admin to perform this task');
         }
 
         next();
